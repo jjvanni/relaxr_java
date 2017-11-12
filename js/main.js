60dd8f4eb67cf6c1cfc2ab6e788db865c5433fed
JJ Vanni <jjvanni2@gmail.com>
2017-11-11 19:08:54 -0800

    RelaxR-Java
    11/11 7:00

--- js/main.js
+++ js/main.js
@@ -1,1 +1,38 @@
-alert('hi');
+$('.hide').hide();
+$('.rllink').hide();
+
+
+$(document).ready(function() {
+	$('#readmore1').click(function() {
+		$('#show-on-click1').slideDown();
+		$('#readmore1').hide();
+    	$('#readless1').show();
+    	event.preventDefault();
+	})
+	$('#readless1').click(function(){
+	    $('#show-on-click1').slideUp();
+	    $('#readless1').hide();
+	    $('#readmore1').show();
+	    event.preventDefault();
+	});
+	$('#readmore2').click(function() {
+		$('#show-on-click2').slideDown();
+		$('#readmore2').hide();
+    	$('#readless2').show();
+    	event.preventDefault();
+	})
+	$('#readless2').click(function(){
+	    $('#show-on-click2').slideUp();
+	    $('#readless2').hide();
+	    $('#readmore2').show();
+	    event.preventDefault();
+	});
+	$('#learnmore').click(function(){
+		$('#aboutmore').slideDown();
+		$('#learnmore').hide();
+		event.preventDefault();
+	});
+});
+
+
+
