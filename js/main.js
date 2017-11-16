0f1b864d5f274667519ee96971765177678fcd05
JJ Vanni <jjvanni2@gmail.com>
2017-11-15 17:59:11 -0800

    RelaxR-Java
    11/15 6:00pm

--- js/main.js
+++ js/main.js
@@ -3,31 +3,31 @@ $('.rllink').hide();
 
 
 $(document).ready(function() {
-	$('#readmore1').click(function() {
+	$('#readmore1').click(function(event) {
 		$('#show-on-click1').slideDown();
 		$('#readmore1').hide();
     	$('#readless1').show();
     	event.preventDefault();
 	})
-	$('#readless1').click(function(){
+	$('#readless1').click(function(event){
 	    $('#show-on-click1').slideUp();
 	    $('#readless1').hide();
 	    $('#readmore1').show();
 	    event.preventDefault();
 	});
-	$('#readmore2').click(function() {
+	$('#readmore2').click(function(event) {
 		$('#show-on-click2').slideDown();
 		$('#readmore2').hide();
     	$('#readless2').show();
     	event.preventDefault();
 	})
-	$('#readless2').click(function(){
+	$('#readless2').click(function(event){
 	    $('#show-on-click2').slideUp();
 	    $('#readless2').hide();
 	    $('#readmore2').show();
 	    event.preventDefault();
 	});
-	$('#learnmore').click(function(){
+	$('#learnmore').click(function(event){
 		$('#aboutmore').slideDown();
 		$('#learnmore').hide();
 		event.preventDefault();
